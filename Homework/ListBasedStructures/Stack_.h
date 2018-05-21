138f9facef0e9970ee315873432a92a841686bec
Dakosia <dana.kerey@gmail.com>
2018-05-22 01:54:04 +0600

    Add ListBasedStructures - hw 21.05.18

---
+++ Homework/ListBasedStructures/Stack_.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "doubleLinkedList.h"
+template<class T>
+class Stack_
+{
+private:
+	doubleLinkedList<T> s;
+public:
+	int size() const;
+	T& getTop();
+	void push(const T & val);
+	void pop();
+	bool empty() const;
+};
+
